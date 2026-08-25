460f2a2c6ddfe5469dffe077e8c9420fe50fc14c
PixelWave4433 <95622202+PixelWave4433@users.noreply.github.com>
2026-08-25 21:48:35 +0530

    Added initial FastAPI backend
    Tried to use HYCOMModel from model_processor

--- api.py
+++ api.py
@@ -1,10 +1,26 @@
+## created on 25/08/2026 by Manav Gharat
+## I tried my best to add comments TwT
+
 from fastapi import FastAPI
-app = FastAPI( title = "Ocean Data API", description = "Test for SIH", version = "0.0.1" )
+from model_processor import HYCOMModel
+app = FastAPI( title = "Ocean Data API", description = "API For Ocean Data Visualization Platform", version = "1.0.0" )
 
 @app.get("/")
 def root():
-    return {"message": "The API is working"}
+    return {"message": "Ocean Data API is working"}
 
-@app.get("/health")
+@app.get("/health") #just for status
 def health_check():
     return {"status": "ok"}
+
+@app.get("/metadata")
+def metadata():
+    model = HYCOMModel() ## opening the HYCOM NetCDF file
+    metadata = model.metadata() ##calling the method
+    return{
+        "variables": metadata["variables"],
+        "times": [str(time) for time in metadata["times"]],  ## Converted them cuz xarray values won't fly, we need JSON
+        "depths": [float(depth) for depth in metadata["depths"]], ##same as the one above
+        "latitude_range": metadata["latitude_range"],
+        "longitude_range": metadata["longitude_range"],
+    }
